e765f7cd71b35d4acac6ac5984de574ac2c7eb1d
Sithum Nissanka <sithum.nissanka@gmail.com>
2022-11-12 02:55:30 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD37me47bUFdljaDnJXBzjOHtoA5FTqkRG1l74qDOvLh2h5pwq0y50tZ1Qw/F+RWIJKnPNlAKCk83ID3ihRRQl+crI5LvuI/AVu4p7yHVdtVWQshNamgj5arpH78naVdjDF52Q4DHc9W7RrOaLcPA7KJR083EQViQ2s1Ot9oU5aEh0qlTPtg8BzXtZDg8uolzHgRTKWFqqA44kxmakQuNwUiNc1Giee3yYERD+18qyUM/P+DLl9isPa+RwjCBP7oddBACKZzh2bb5dtpDSaY4v4RO6bUDxkSxt6xpgH7oWq3B9TnyA/AuVv+c6owyybxYEEV+3qN+R+4qnyoAnDxP3b',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDC3NiCGfgblgDkZmYT1LD4GRjcFb20W73UG+pgrH9nuF4F2ySDoSXcYZ/zmi+1QIDgHGt5ZaH/FUJyFfbR2s71a4n44nAb9CObvuXnUn3OCMsAowEddspKKXVMUZaawU9+TnEeq3z3zpq9hhBImQbAanJXpyOyKhxSI/0TMtJJySgvvAs+3R0VcCMAC4cS1VHLJutchSi963uYpYAe9uAaPAzHVY6xW28DokD8n5Zs1ukcErO2YbZqwU54q7euTd5pW5ANTLf5hbdSrSaig4XQq09jUzPbyHx7bQp+fpx01GEJSdi6bSlBLxjqt7ALuO0lrqLgYLI8NC2MVO1lcNUD',
 	}  
 }
