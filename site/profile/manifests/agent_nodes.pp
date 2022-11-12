08241ab56a86a245c7a7d18c7affa8388f5dd1e6
Sithum Nissanka <sithum.nissanka@gmail.com>
2022-11-12 03:09:15 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -5,7 +5,7 @@ class profile::agent_nodes {
   dockeragent::node { 'minetest.puppet.vm': }
   host {'web.puppet.vm':
     ensure  => present,
-    ip      => '172.18.0.2',
+    ip      => '172.18.0.4',
   }
   host {'db.puppet.vm':
     ensure  => present,
